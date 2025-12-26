9356634ad244127f15ce570dbf061949b04e354b
Rutvik <rutvik.parmar.2103@gmail.com>
2025-12-26 17:58:53 +0530

    done

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -30,7 +30,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgb(126 152 172)" }}>
             "Strive to build technology that creates meaningful impact."{" "}
           </p>
           <footer className="blockquote-footer">Rutvik</footer>
--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="rutvikp003"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#4399fa"
         fontSize={16}
       />
     </Row>
